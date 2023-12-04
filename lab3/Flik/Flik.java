d2e4b66f41bf9267db6021f0192f1353dfdc6beb
mrcongliu <upcliucong@gmail.com>
2023-12-04 06:12:10 -0500

    Lab 3: Unit Testing with JUnit, Debugging

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,7 +1,6 @@
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
     public static boolean isSameNumber(Integer a, Integer b) {
-        boolean result = a == b;
-        return result;
+        return a == b;
     }
 }
