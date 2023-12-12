fdebc248039950272cf67458426c67f71c6e0583
mrcongliu <upcliucong@gmail.com>
2023-12-12 16:47:41 -0500

    hotfix proj1b

--- proj1b/TestOffByN.java
+++ proj1b/TestOffByN.java
@@ -4,10 +4,14 @@ import static org.junit.Assert.*;
 
 public class TestOffByN {
     OffByN offBy5 = new OffByN(5);
+    OffByN offBy4 = new OffByN(4);
 
     @Test
     public void testEqualChars() {
-        assertFalse(offBy5.equalChars('a', 'F'));
+        assertTrue(offBy4.equalChars('A', 'E'));
+        assertFalse(offBy4.equalChars('A', 'D'));
+        assertFalse(offBy4.equalChars('%', '&'));
+        assertTrue(offBy5.equalChars('A', 'F'));
         assertFalse(offBy5.equalChars('%', '&'));
         assertTrue(offBy5.equalChars('a', 'f'));
         assertTrue(offBy5.equalChars('f', 'a'));
