43a640729a24d8aaa840fe2310a48b691d416fab
mrcongliu <upcliucong@gmail.com>
2023-12-13 07:10:15 -0500

    hotfix proj1b

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,6 +1,6 @@
 public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y) {
-        return Math.abs(x - y) == 1;
+        return Math.abs(x - y) == 1 || Math.abs(x - y) == ('a' - 'B');
     }
 }
