77d7ea6859ba69c0c8fb07cbe6cbb954fb632d14
mrcongliu <upcliucong@gmail.com>
2023-12-13 11:34:12 -0500

    i hope completed proj1b

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,6 +1,6 @@
 public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y) {
-        return Math.abs(x - y) == 1 || Math.abs(x - y) == 33 || Math.abs(x - y) == 31;
+        return Math.abs(x - y) == 1;
     }
 }
--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -9,8 +9,6 @@ public class TestOffByOne {
     public void testEqualChars() {
         assertTrue(offByOne.equalChars('a', 'b')); // True for lowercase
         assertTrue(offByOne.equalChars('b', 'a')); // True for lowercase
-        assertTrue(offByOne.equalChars('a', 'B')); // True for uppercase
-        assertTrue(offByOne.equalChars('A', 'b')); // True for uppercase
         assertTrue(offByOne.equalChars('A', 'B')); // True for uppercase
         assertTrue(offByOne.equalChars('B', 'A')); // True for uppercase
 
@@ -18,6 +16,8 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('c', 'a')); // False for lowercase
         assertFalse(offByOne.equalChars('A', 'C')); // False for uppercase
         assertFalse(offByOne.equalChars('C', 'A')); // False for uppercase
+        assertFalse(offByOne.equalChars('a', 'B')); // False for one upper and one lower
+        assertFalse(offByOne.equalChars('A', 'b')); // False for one upper and one lower
 
         // Test non-letter characters
         assertTrue(offByOne.equalChars('%', '&'));
